ad7352710977cae4061ffa46d8d256e1f86be302
DavidASC20 <68346945+DavidASC20@users.noreply.github.com>
2022-01-10 20:51:39 -0500

    finished v2

--- hw18.c
+++ hw18.c
@@ -1,52 +1,49 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
-#define READ 0
-#define WRITE 1
+#include <string.h>
 
-int main() {
-	int cPipe[2];
-	int pPipe[2];
-	pipe(cPipe);
-	pipe(pPipe);
+int main(){
+	int fds[2];
+	pipe(fds);
 
-	int pid = fork();
+	int fds2[2];
+	pipe(fds2);
 
-	if (pid == -1) {
-		printf("Error, Message is: %s\n", strerror(errno));
-		return 1;
-	}
+	int forker = fork();
 
-	if (pid == 0) {
-		close(cPipe[WRITE]);
-		close(pPipe[READ]);
 
-		char str[10000];
-		int x = -1;
-		while (x != 0) {
-    
-			x = read(cPipe[READ], str, sizeof(str));
-			int i;
-			for (i = 0; i < strlen(str); i++) {
-				if (str[i] >= 65 && str[i] <= 90) str[i] += 32;
-			}
-			write(pPipe[WRITE], str, strlen(str));
+	if (forker){
+		close(fds[0]);
+		close(fds2[1]);
+		while (1){
+			char * message = calloc(1000, 1);
+			printf("input: \n");
+			fgets(message, 1000, stdin);
+			write(fds[1], message, strlen(message));
+			read(fds2[0], message, 1000);
+			printf("manipulated B): %s\n", message);
+			free(message);
 		}
 	} else {
-		close(pPipe[WRITE]);
-		close(cPipe[READ]);
+		close(fds[1]);
+		close(fds2[0]);
+		while (1){
+			char * message = calloc(1000, 1);
+			int thing = read(fds[0], message, 1000);
+			char * temp = calloc(1000, 1);
 
-		char str[10000];
-		while (1 == 1) {
-			printf("Uppercase String: ");
-			str[0] = '\0';
-			fgets(str, sizeof(str), stdin);
-			if (strlen(str) == 0) return 0;
-			write(cPipe[WRITE], str, sizeof(str));
-			read(pPipe[READ], str, sizeof(str));
-			printf("Returned lowercase: %s", str);
+			int i;
+			for (i = strlen(message) - 1; i >= 0; i--){
+				if (*(message + i) != 10){
+					strncat(temp, message + i, 1);
+				}
+			}
+
+			write(fds2[1], temp, 1000);
+			free(message);
+			free(temp);
 		}
 	}
-}
+
+	return 0;
